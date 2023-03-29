36be6d81ba2cd4265e927cacc8aa20ff7c18366b
Vuyani00 <nkosivuyani03@gmail.com>
2023-03-28 23:37:19 -0700

    Printf

--- main.h
+++ main.h
@@ -42,5 +42,9 @@ typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
-
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_int(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[], int flags, int width, int precision, int size);
 #endif
